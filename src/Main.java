654c83dd858c1196a11a600ed0d979869c7fbc64
alexkub22 <alexkub22@yandex.ru>
2019-11-24 23:31:49 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public String contact(String[] strings) {
+        String string= "";
+        for (int i=0; i<string.length(); i++){
+            string+= strings[i]+""
+        }
+        return string;
+    }
+}
